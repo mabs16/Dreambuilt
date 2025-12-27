0934f789da1a83f5fdca96189383ac449b5a2651
Mario Bustamante <bustamantemario67@gmail.com>
2025-12-27 16:32:43 -0500

    deploy: add Dockerfile and configure port for Google Cloud Run

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 8080;
   await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 void bootstrap();
